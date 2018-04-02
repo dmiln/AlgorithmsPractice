b2b379b890ee965cd697b43abf1b59fdcfc35d97
Dmitry <dmitilyin@gmail.com>
2018-04-02 18:06:05 +0300

    пробую очередь

---
+++ src/greedyalgorithms/prioityqueue/PriorityQueue.java
@@ -0,0 +1,65 @@
+package greedyalgorithms.prioityqueue;
+
+import java.util.Scanner;
+
+public class PriorityQueue {
+
+    public static void main(String[] args) {
+        System.out.println(scanInstructions());
+    }
+
+    private static String scanInstructions() {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder instructions = new StringBuilder();
+        int numOfInstr = scanner.nextInt();
+        while (scanner.hasNext()) {
+            String s = scanner.nextLine();
+            instructions.append(s).append("\n");
+            numOfInstr--;
+            if (numOfInstr < 0) {
+                break;
+            }
+        }
+        return instructions.toString();
+    }
+
+    private static String getMethod(String s, int i) {
+        String[] str = s.split("\n");
+        if (i < str.length) {
+            return str[i].split(" ")[0];
+        } else {
+            return "error";
+        }
+    }
+
+    private static int getValue(String s, int i) {
+        String[] str = s.split("\n");
+        if (i < str.length) {
+            return Integer.parseInt(str[i].split(" ")[1]);
+        } else {
+            return 0;
+        }
+
+    }
+
+    private static void run() {
+        String s = scanInstructions();
+        for (int i =0; i < s.split("\n").length; i++){
+            String method = getMethod(s, i);
+            if (method.equals("Insert")){
+                int value = getValue(s, i);
+                insert(value);
+            }else if(method.equals("ExtractMax")){
+                extractMax();
+            }
+        }
+    }
+
+    private static void extractMax() {
+
+    }
+
+    private static void insert(int value) {
+
+    }
+}
